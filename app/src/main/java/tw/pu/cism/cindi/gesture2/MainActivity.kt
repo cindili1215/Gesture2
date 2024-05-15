1f71d005b21de48638bdca29048261038892b031
cindili1215 <cindy1050286@gamil.com>
2024-05-15 15:50:18 +0800

    v0

--- app/src/main/java/tw/pu/cism/cindi/gesture2/MainActivity.kt
+++ app/src/main/java/tw/pu/cism/cindi/gesture2/MainActivity.kt
@@ -32,7 +32,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello cindi!",
+        text = "Hello cindy!",
         modifier = modifier
     )
 }
